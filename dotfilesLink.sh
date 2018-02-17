03813e3a018ba89da6147a9717492c354288f1df
teruyuki suda <sutemail0118@gmail.com>
2018-02-17 13:33:05 +0900

    .tmux.conf .vrapperrcを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/indent ~/.vim
