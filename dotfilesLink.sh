3a5bff065a15e0e19ebb5ac526706aa1f131c318
teruyuki suda <sutemail0118@gmail.com>
2018-03-28 23:43:56 +0900

    shellの間違い修正

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/indent ~/.vim
