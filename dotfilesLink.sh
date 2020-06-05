c00c815c5b0910d3667be41d7ff3ced00314dfd4
tsuda <tsuda@healthcare.ne.jp>
2020-06-05 23:44:05 +0900

    modified dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
 unlink ~/.vim/colors
 ln -sf ~/dotfiles/colors ~/.vim/colors
 unlink ~/.vim/indent
