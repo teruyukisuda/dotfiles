fe710edf6ec44158980a7acdcb1ab346fe8faf4c
tsuda <tsuda@healthcare.ne.jp>
2020-06-05 20:32:39 +0900

    add

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,6 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/indent ~/.vim/indent
+ln -sf ~/dotfiles/autoload ~/.vim/autoload
+# deinVimのtomlファイル
+ln -sf ~/dotfiles/rc ~/.vim/rc
