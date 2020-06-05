fb2a42913a21862d1393a8ea1e7b0d601d6c2fb5
tsuda <tsuda@healthcare.ne.jp>
2020-06-05 18:39:12 +0900

    .ideavimrcの追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
