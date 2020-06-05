02cc4ac46c422be68da983a5f482b0fe46adcd1e
tsuda <tsuda@healthcare.ne.jp>
2020-06-05 20:39:09 +0900

    dotfilesLink.sh修正　unlinkしてからlinkする

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,8 +5,12 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+unlink ~/.vim/colors
 ln -sf ~/dotfiles/colors ~/.vim/colors
+unlink ~/.vim/indent
 ln -sf ~/dotfiles/indent ~/.vim/indent
+unlink ~/.vim/autoload
 ln -sf ~/dotfiles/autoload ~/.vim/autoload
 # deinVimのtomlファイル
+unlink ~/.vim/rc
 ln -sf ~/dotfiles/rc ~/.vim/rc
